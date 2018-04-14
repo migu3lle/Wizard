d41818a7c076506afc336da71030c209f98b543a
Christian Egger <chreggermx.at>
2018-04-14 11:33:07 +0200

    #4 - CreateGame + LaunchGame Button function added

--- app/src/main/java/aau/losamigos/wizard/MainActivity.java
+++ app/src/main/java/aau/losamigos/wizard/MainActivity.java
@@ -1,7 +1,10 @@
 package aau.losamigos.wizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button createGame = findViewById(R.id.createGame);
+        createGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createGame(createGame);
+            }
+        });
+
+        final Button launchGame = findViewById(R.id.launchGame);
+        launchGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchGame(launchGame);
+            }
+        });
+
+    }
+    public void createGame(View view){
+        Intent intent = new Intent(this, CreateGameActivity.class);
+        startActivity(intent);
+    }
+    public void launchGame(View view){
+        Intent intent = new Intent(this, LaunchGameActivity.class);
+        startActivity(intent);
     }
 }
