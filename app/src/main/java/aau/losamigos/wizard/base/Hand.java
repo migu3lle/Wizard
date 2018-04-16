518d3bb625007dc25de0fc161e0dd60fa0b9c5a9
andimairitsch <anmairitsch@edu.aau.at>
2018-04-16 14:13:09 +0200

    added handowner in class hand
    hand gets a owner (player)
    remove hand from class player

--- app/src/main/java/aau/losamigos/wizard/base/Hand.java
+++ app/src/main/java/aau/losamigos/wizard/base/Hand.java
@@ -9,9 +9,22 @@ import aau.losamigos.wizard.elements.Player;
 public class Hand {
 
     private AbstractCard handCards[];
+    private Player handOwner;
 
     public Hand(AbstractCard[] handCards) {
         this.handCards = handCards;
+    }
+    public Hand(AbstractCard[] handCards, Player handOwner) {
+        this.handCards = handCards;
+        this.handOwner = handOwner;
+    }
+
+    public Player getHandOwner() {
+        return handOwner;
+    }
+
+    public void setHandOwner(Player handOwner) {
+        this.handOwner = handOwner;
     }
 
 
