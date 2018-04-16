a8bc921330e3e6063879924c1d2c4a01d4685405
andimairitsch <anmairitsch@edu.aau.at>
2018-04-16 16:56:50 +0200

    #16 implemented getAllowedCards
    getAllowedCards schaut welche Handkarten zu spielen erlaubt sind oder nicht.
    array handCards wurde auf eine ArrayList abgeändert

--- app/src/main/java/aau/losamigos/wizard/base/Hand.java
+++ app/src/main/java/aau/losamigos/wizard/base/Hand.java
@@ -1,6 +1,12 @@
 package aau.losamigos.wizard.base;
 
+import java.util.ArrayList;
+
 import aau.losamigos.wizard.elements.Player;
+import aau.losamigos.wizard.elements.cards.FractionCard;
+import aau.losamigos.wizard.elements.cards.JesterCard;
+import aau.losamigos.wizard.elements.cards.WizardCard;
+import aau.losamigos.wizard.types.Fractions;
 
 /**
  * Created by Andreas.Mairitsch on 16.04.2018.
@@ -8,13 +14,13 @@ import aau.losamigos.wizard.elements.Player;
 
 public class Hand {
 
-    private AbstractCard handCards[];
+    private ArrayList<AbstractCard> handCards;
     private Player handOwner;
 
-    public Hand(AbstractCard[] handCards) {
+    public Hand(ArrayList<AbstractCard> handCards) {
         this.handCards = handCards;
     }
-    public Hand(AbstractCard[] handCards, Player handOwner) {
+    public Hand(ArrayList<AbstractCard> handCards, Player handOwner) {
         this.handCards = handCards;
         this.handOwner = handOwner;
     }
@@ -28,18 +34,78 @@ public class Hand {
     }
 
 
-    public AbstractCard[] getHandCards() {
+    public ArrayList<AbstractCard> getHandCards() {
 
         return handCards;
     }
 
-    public AbstractCard[] getAllowedCards(AbstractCard[] table) {
+    public ArrayList<AbstractCard> getAllowedCards(ArrayList<AbstractCard> table) {
 
-        for (AbstractCard card:handCards) {
-            //TODO implementation: is card allowed to play or not?
+        AbstractCard cardTyp = null; //gespielter Kartentyp
+        Fractions fraction = null; //gespielte Fraktion
 
+        /*
+        Wenn noch keine Karte gespielt, alle Karten erlaubt
+        sonst prüfe ausgespielten Kartentyp und Fraktion
+         */
+        if(table.isEmpty()){
+            for (AbstractCard cardHand:handCards)
+                cardHand.setAllowedToPlay(true);
         }
+        else{
+            /* Fraktionsermittlung
 
+            Prüft table welche Fraktion gespielt werden darf
+            Wenn Zaubererkarte sind alle Handkarten erlaubt
+            Wenn Narr wird nächste Tablekarte geprüft, wenn keine weitere Karte vorhanden
+            sind alle Karten erlaubt.
+            Wenn Fraktion sind nur Zauberer, Narren und Karten der Fraktion erlaubt
+ception arts            */
+            for (AbstractCard cardTable:table) {
+                cardTyp = cardTable;
+                if(cardTyp instanceof WizardCard)
+                    break;
+                else if(cardTyp instanceof JesterCard){
+
+                }else if(cardTyp instanceof FractionCard){
+                    fraction = ((FractionCard) cardTyp).getFraction();
+                    break;
+                }
+                else{
+                    //TODO Exception werfen da kein richtiger Typ
+                }
+            }
+            /*
+            Aufgrund der Fraktioninfo werden die Handkarten jetzt einzeln auf allowedToPlay = true
+            oder false gesetzt.
+            Zauberer und Narren immer erlaubt, Fraktion wird eingeschränkt
+            */
+            if(cardTyp instanceof WizardCard){
+                for (AbstractCard cardHand:handCards)
+                    cardHand.setAllowedToPlay(true);
+            }
+            else if(cardTyp instanceof JesterCard){
+                for (AbstractCard cardHand:handCards)
+                    cardHand.setAllowedToPlay(true);
+
+            }else if(cardTyp instanceof FractionCard){
+                for (AbstractCard cardHand:handCards) {
+                    if(cardHand instanceof WizardCard)
+                        cardHand.setAllowedToPlay(true);
+                    else if(cardHand instanceof JesterCard){
+                        cardHand.setAllowedToPlay(true);
+                    }else if(cardHand instanceof FractionCard){
+                        if(((FractionCard) cardHand).getFraction().equals(fraction))
+                            cardHand.setAllowedToPlay(true);
+                        else
+                            cardHand.setAllowedToPlay(false);
+                    }
+                }
+            }
+            else{
+                //TODO Exception werfen da kein richtiger Typ
+            }
+        }
         return handCards;
     }
 }
