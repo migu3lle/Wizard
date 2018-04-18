383a884bbcfeb7ea99a270f4de1ee5098101ad63
andimairitsch <anmairitsch@edu.aau.at>
2018-04-18 15:17:21 +0200

    added class round

---
+++ app/src/main/java/aau/losamigos/wizard/base/Round.java
@@ -0,0 +1,25 @@
+package aau.losamigos.wizard.base;
+
+import java.util.ArrayList;
+
+import aau.losamigos.wizard.elements.Player;
+
+/**
+ * Created by Andreas.Mairitsch on 18.04.2018.
+ */
+
+public class Round {
+
+    private ArrayList<Player> players;
+    private ArrayList<Hand> hands;
+
+    private ArrayList<AbstractCard> tables;
+
+    
+
+    private void generateHands(){
+
+    }
+
+
+}
