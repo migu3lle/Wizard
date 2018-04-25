6d36f90758c55b2e141862db0affbe7838e7ae49
andimairitsch <anmairitsch@edu.aau.at>
2018-04-25 15:21:39 +0200

    #22 getPlayerHand and playCard

--- app/src/main/java/aau/losamigos/wizard/base/Round.java
+++ app/src/main/java/aau/losamigos/wizard/base/Round.java
@@ -1,5 +1,6 @@
 package aau.losamigos.wizard.base;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 
 import aau.losamigos.wizard.elements.Player;
@@ -11,6 +12,7 @@ import aau.losamigos.wizard.elements.Player;
 public class Round {
 
     private ArrayList<Player> players; //TODO eventuell in die Gameconfig verschieben
+    private CardStack cardStack;
     private ArrayList<Hand> hands;
     private int rounds;
     private int trump;
@@ -18,10 +20,21 @@ public class Round {
 
     private ArrayList<AbstractCard> table;
 
+    public Round(ArrayList<Player> players, int rounds) {
+
+        this.players = players;
+        this.cardStack = new CardStack();
+        this.hands = new ArrayList<Hand>();
+        this.rounds = rounds;
+        this.trump = cardStack.getTrump();
+        this.playerNumber = players.size();
+
+        generateHands();
+    }
 
     private void generateHands(){
 
-        ArrayList<AbstractCard> cards = getCards(rounds*playerNumber);
+        ArrayList<AbstractCard> cards = cardStack.getCards(rounds*playerNumber);
 
         int index = 0;
         for (Player player:players) {
@@ -30,11 +43,32 @@ public class Round {
             hands.add(new Hand(newHandcards));
         }
 
-
-
-
+    }
+    private void cleanHands(){
+        hands.clear();
+    }
+    public AbstractList<AbstractCard> getPlayerHand(Player player){
+        for (Hand hand: hands) {
+            if(hand.getHandOwner().equals(player))
+                return hand.getAllowedCards(table); //TODO oder nur die IDs geben
+        }
+        return null;
     }
 
+    public void playCard(Player player, int cardID) throws Exception{
+        for (Hand hand: hands) {
+            if(hand.getHandOwner().equals(player)){
+                for (AbstractCard card:hand.getAllowedCards(table)) {
+                        if (card.getId()==cardID)
+                            table.add(card);
+                        else
+                            throw new Exception();
+                }
+            }
+
+        }
+
+    }
 
 
 }
