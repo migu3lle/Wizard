c99f53b676f1b7b8e6974186e3100723dd63f5a3
Christian Egger <chreggermx.at>
2018-05-01 19:00:09 +0200

    Function playCard for each player

--- app/src/main/java/aau/losamigos/wizard/TableActivity.java
+++ app/src/main/java/aau/losamigos/wizard/TableActivity.java
@@ -1,14 +1,44 @@
 package aau.losamigos.wizard;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TableActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
         getSupportActionBar().hide();
+
+    }
+
+   public void playCard1(View card){
+        card.setX(850);
+        card.setY(500);
+   }
+   public void playCard2(View card){
+       card.setX(700);
+       card.setY(350);
+   }
+    public void playCard3(View card){
+        card.setX(800);
+        card.setY(300);
+    }
+    public void playCard4(View card){
+        card.setX(900);
+        card.setY(250);
+    }
+    public void playCard5(View card){
+        card.setX(1000);
+        card.setY(300);
+    }
+    public void playCard6(View card){
+        card.setX(1100);
+        card.setY(350);
     }
 }
